e039d8fd886de3a2c792eb76c52459bb637a489d
mhmmtalifdn <mhmmtalifdn@gmail.com>
2024-11-23 21:59:56 +0300

    Implement updating and deleting features

--- resources/js/Components/FeatureItem.tsx
+++ resources/js/Components/FeatureItem.tsx
@@ -1,6 +1,7 @@
 import { Feature } from "@/types";
 import { Link } from "@inertiajs/react";
 import { useState } from "react";
+import FeatureActionsDropdown from "./FeatureActionsDropdown";
 
 export default function FeatureItem({ feature }: { feature: Feature }) {
     const [isExpanded, setIsExpanded] = useState(false);
@@ -48,6 +49,9 @@ export default function FeatureItem({ feature }: { feature: Feature }) {
                             <p>{feature.description}</p>
                         </>
                     )}
+                </div>
+                <div>
+                    <FeatureActionsDropdown feature={feature} />
                 </div>
             </div>
         </div>
--- resources/js/Pages/Feature/Edit.tsx
+++ resources/js/Pages/Feature/Edit.tsx
@@ -4,15 +4,16 @@ import PrimaryButton from '@/Components/PrimaryButton';
 import TextAreaInput from '@/Components/TextAreaInput';
 import TextInput from '@/Components/TextInput';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import type { Feature } from '@/types';
 import { Head, useForm } from '@inertiajs/react';
 import { FormEventHandler } from 'react';
 
-export default function Create() {
-    const { data, setData, processing, errors, post } = useForm({ name: '', description: '' })
+export default function Edit({ feature }: { feature: Feature }) {
+    const { data, setData, processing, errors, put } = useForm({ name: feature.name, description: feature.description })
 
-    const createFeature: FormEventHandler = (ev) => {
+    const updateFeature: FormEventHandler = (ev) => {
         ev.preventDefault();
-        post(route('feature.store'), {
+        put(route('feature.update', feature.id), {
             preserveScroll: true
         });
     }
@@ -21,15 +22,15 @@ export default function Create() {
         <AuthenticatedLayout
             header={
                 <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-                    Create New Feature
+                    Edit Feature <b>"{feature.name}"</b>
                 </h2>
             }
         >
-            <Head title="Create New Feature" />
+            <Head title={"Edit Feature " + feature.name}/>
 
             <div className="mb-4 overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
                 <div className="p-6 text-gray-900 dark:text-gray-100 flex gap-8">
-                    <form onSubmit={createFeature} className='w-full'>
+                    <form onSubmit={updateFeature} className='w-full'>
 
                         <div className='mb-8'>
                             <InputLabel htmlFor="name" value="Name" />
@@ -60,7 +61,7 @@ export default function Create() {
 
                             <InputError className="mt-2" message={errors.description} />
                         </div>
-                        
+
                         <div className="flex items-center gap-4">
                             <PrimaryButton disabled={processing}>Save</PrimaryButton>
                         </div>
