0cf2c7993eeb74533b36472fe1916019063e0695
mhmmtalifdn <mhmmtalifdn@gmail.com>
2024-11-24 14:03:24 +0300

    Implement upvote and downvote

--- app/Http/Controllers/UpvoteController.php
+++ app/Http/Controllers/UpvoteController.php
@@ -2,64 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Feature;
 use App\Models\Upvote;
 use Illuminate\Http\Request;
 
 class UpvoteController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Feature $feature)
     {
-        //
-    }
+        $data = $request->validate([
+            'upvote' => ['required', 'boolean']
+        ]);
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Upvote $upvote)
-    {
-        //
-    }
+        Upvote::updateOrCreate(
+            ['feature_id' => $feature->id, 'user_id' => auth()->id()],
+            ['upvote' => $data['upvote']]
+        );
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Upvote $upvote)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Upvote $upvote)
-    {
-        //
+        return back();
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Upvote $upvote)
+    public function destroy(Feature $feature)
     {
-        //
+        $feature->upvotes()->where('user_id', auth()->id())->delete();
+
+        return back();
     }
 }
